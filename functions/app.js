66d1aceb62d050ff6d6eac606b9a08556c2a629f
eljefe-martin <jeffry.martin@gmail.com>
2020-02-25 15:22:39 -0700

    added lead summary logic

--- functions/app.js
+++ functions/app.js
@@ -90,3 +90,68 @@ function yearMonth(created){
     }    
     //fs.writeFileSync('./agencySummary.json',JSON.stringify(summary),'utf8')
 })()    
+
+/****/
+const db = [
+    {agency: 'a1', id: 12345, created: "2020-01-05", stage: 'Leads', status: 'open', pd:{rate:"", newRate:""}},
+    {agency: 'a1', id: 22345, created: "2020-01-05", stage: 'Presenting', status: 'open', pd:{rate:"1200", newRate:""}},
+    {agency: 'a1', id: 32345, created: "2020-01-05", stage: 'Closed', status: 'won', pd:{rate:"1000", newRate:"950"}},
+    {agency: 'a1', id: 42345, created: "2020-01-05", stage: 'Closed', status: 'won', pd:{rate:"1500", newRate:"1300"}},
+    {agency: 'a1', id: 52345, created: "2020-01-05", stage: 'Verified', status: 'open', pd:{rate:"500", newRate:""}},
+    {agency: 'a1', id: 62345, created: "2020-01-05", stage: 'Quoting', status: 'open', pd:{rate:"900", newRate:""}},
+    {agency: 'a2', id: 12341, created: "2020-01-05", stage: 'Leads', status: 'open', pd:{rate:"1200", newRate:""}},
+    {agency: 'a2', id: 12342, created: "2020-01-05", stage: 'Presenting', status: 'open', pd:{rate:"500", newRate:"400"}},
+    {agency: 'a2', id: 12343, created: "2020-01-05", stage: 'Presenting', status: 'open', pd:{rate:"700", newRate:"500"}},
+    {agency: 'a2', id: 12344, created: "2020-02-05", stage: 'Closed', status: 'won', pd:{rate:"1000", newRate:"800"}},
+    {agency: 'a2', id: 12345, created: "2020-02-05", stage: 'Closed', status: 'archive', pd:{rate:"1000", newRate:"800"}},
+    {agency: 'a2', id: 12346, created: "2020-02-05", stage: 'Closed', status: 'archive', pd:{rate:"1000", newRate:"800"}},
+]
+
+function yearMonth(created) {
+    try {
+        let dt = '0' + created.toLocaleDateString()
+        let dtArray = dt.split('/')
+        return dtArray[2] + dtArray[0].slice(-2)
+    } catch (error) {
+        console.error(error.message)
+    }
+}
+
+function leadAmount(pd){
+    return pd.newRate ? parseFloat(pd.newRate) : pd.rate ? parseFloat(pd.rate) : 0
+}
+
+function createHeader(leads){
+    try {
+        return leads.reduce((obj, item) => {
+            //if new agency add a key and an object
+            if(!obj[item.agency]){
+                obj[item.agency] = {}
+            }
+            //if stage is missing add and seed
+            if(!obj[item.agency][item.stage]){
+                obj[item.agency][item.stage] = {
+                    count: 1,
+                    amount: leadAmount(item.pd)
+                }
+            } else {
+                //existing stage summaryize
+                //if the stage is closed we only deal with status === 'won'
+                if(item.stage !== 'Closed' || (item.stage === 'Closed' && item.status === 'won')){
+                    obj[item.agency][item.stage].count ++
+                    obj[item.agency][item.stage].amount += leadAmount(item.pd)
+                }
+            }
+            return obj
+        },{})
+    } catch (error) {
+        console.error(error.message)
+    }
+
+}
+
+
+;(() => {
+    console.log(createHeader(db))
+    //console.log(yearMonth(new Date("2020-01-05")))
+})()
